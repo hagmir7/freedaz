4c5a88395d68d53208291c0544084a3b00701678
Agmir Hassane <hagmir6@gmail.com>
2023-06-30 15:10:46 +0100

    my new commit

--- video/sites/best.py
+++ video/sites/best.py
@@ -214,7 +214,7 @@ def getItem(url, image, title):
 
 
 def best(request):
-    for page in range(95, 0, -1):
+    for page in range(93, 0, -1):
         url = f"https://weciimaa.online/seriestv/best/?page_number={page}/"
         html = requests.get(url)
         soup = BeautifulSoup(html.content, "html.parser")
